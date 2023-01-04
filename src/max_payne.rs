589f8e56fc6a6bae685467d98e91e5d5412034c0
mohkelmohamady <moh.k.elmohamady@gmail.com>
2023-01-04 17:21:55 +0100

    Added two functions to max_payne module to parse the response and the error from twitter when sending the request

--- src/max_payne.rs
+++ src/max_payne.rs
@@ -47,3 +47,45 @@ pub async fn spawn_max_payne() -> anyhow::Result<MaxPayneBot> {
     })
 }
 
+pub async fn parse_tweet_from_response(response_body: String) -> anyhow::Result<Tweet> {
+    // Parse the string from json body
+    let tweet_response_json_blob: Value = serde_json::from_str(&response_body)?;
+    println!("{:?}", &tweet_response_json_blob);
+    // Take out the id and the text of the tweet
+    // https://stackoverflow.com/questions/27043268/convert-a-string-to-int
+    let id: i64 = tweet_response_json_blob["data"]["id"]
+        .to_string()
+        .replace("\\", "")
+        .replace("\"", "")
+        .parse::<i64>()?;
+    let text: String = tweet_response_json_blob["data"]["text"].to_string();
+
+    println!(
+        "Successfully parsed the quote with the id {:?} and the text is {:?}",
+        id, text
+    );
+
+    return Ok(Tweet { id, text});
+}
+
+pub async fn parse_unsucessful_request(response : TwitterResponse) -> anyhow::Result<UnSuccessfulTweetStatus> {
+    match response {
+        TwitterResponse::Response(r) => {
+            
+            let mut serialized_headers : String = String::from("");
+            for header in r.headers().clone().iter() {
+                serialized_headers.push_str(format!("{}:{:?} \n", header.0, header.1).as_str())
+            }
+          
+            let tweet_response_json_blob: Value = serde_json::from_str(r.text().await.unwrap().as_str())?;
+            let failure_reason : String = tweet_response_json_blob["title"].to_string().replace("\\", "").replace("\"", "");
+            let status_code  = tweet_response_json_blob["status"].as_i64().unwrap();
+            println!("Parsed the unsucessfull response from tweet");
+            Ok(UnSuccessfulTweetStatus{failure_reason : failure_reason, serialized_headers : serialized_headers, status_code : status_code})
+        }
+        TwitterResponse::FailedResponse(f) => {
+            println!("Failed request {:?}", f);
+            Ok(UnSuccessfulTweetStatus { status_code: 404, failure_reason: String::from("Error"), serialized_headers: String::from("Error")})
+        }
+    }
+}
