420d3abc5c7ae6ba9ebd0489c354d28b169b808b
mohkelmohamady <moh.k.elmohamady@gmail.com>
2023-01-05 14:01:09 +0100

    Added to the main module an async task that will make max payne tweet every three hours

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,9 @@
 pub mod cassandra;
 pub mod max_payne;
 pub mod parser;
-
-use cassandra::initialise_database_connection;
 use dotenv::dotenv;
 use max_payne::spawn_max_payne;
-use stargate_grpc::{client::StargateClientBuilder, *};
-use std::str::FromStr;
+use stargate_grpc::*;
 use tokio::{time::sleep, task};
 
 #[tokio::main]
@@ -14,5 +11,18 @@ async fn main() -> anyhow::Result<()> {
     // Load enviroment variables from env
     dotenv().ok();
 
+    task::spawn(async {
+        let mut max_payne = spawn_max_payne().await.unwrap();
+
+        max_payne.create_tables_if_not_exists().await.unwrap();
+
+        parser::parse_quotes(&mut max_payne).await.unwrap();
+
+        loop {
+            sleep(tokio::time::Duration::from_secs(3600 * 3)).await;
+            max_payne.tweet_a_quote().await.unwrap();
+        }
+    }).await?;
+    
     return Ok(())
 }
